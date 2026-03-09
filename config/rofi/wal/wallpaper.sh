792372d2137d9a643c357b2d2f18847347525383
Cloudnine-09 <cloudnine895473@gmail.com>
2026-03-10 03:51:25 +0530

    Added rofi

---
+++ config/rofi/wal/wallpaper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+WALL_DIR="$HOME/.config/wallpapers"
+ROFI_THEME="$HOME/.config/rofi/wal/wallpaper.rasi"
+
+# fetch all wallpapers
+mapfile -d '' files_full_path < <(find "$WALL_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg' \) -print0)
+
+# prepare them as rofi input
+SELECTED_FILENAME=$(
+    for path in "${files_full_path[@]}"; do
+        filename="${path##*/}"
+        printf "%s\0icon\x1f%s\n" "$filename" "$path"
+    done | rofi -dmenu -replace -config "$ROFI_THEME" -show-icons
+)
+
+# get the selected item full path
+if [ -n "$SELECTED_FILENAME" ]; then
+    SELECTED_PATH=""
+    for path in "${files_full_path[@]}"; do
+        if [[ "${path##*/}" == "$SELECTED_FILENAME" ]]; then
+            SELECTED_PATH="$path"
+            break
+        fi
+    done
+
+    # execute theme script
+    if [ -n "$SELECTED_PATH" ]; then
+        "$HOME/.config/rofi/wal/theme.sh" "$SELECTED_PATH"
+    fi
+fi
